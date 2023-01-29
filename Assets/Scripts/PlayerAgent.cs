e3903be5ea326ab44a6d291e334f1a0c99dfdd31
Andrew Magnuson <ajwm8103@gmail.com>
2023-01-29 01:26:17 -0500

    Folder stuff

---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class PlayerAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
